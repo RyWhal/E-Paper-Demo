df236962542d971f2b7f7ea70e579345039c2fbc
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-25 13:21:38 -0500

    Update typing_example.py

--- typing_example.py
+++ typing_example.py
@@ -3,14 +3,13 @@ import os
 import logging
 from waveshare_epd import epd4in2_V2
 from PIL import Image,ImageDraw,ImageFont
-from pynput import keyboard
-#import keyboard
+import keyboard
 
 logging.basicConfig(level=logging.DEBUG)
 logging.info("epd4in2 Demo")
 
 logging.info("set font object")
-font18 = ImageFont.truetype('Font.ttc', 12)
+font18 = ImageFont.truetype('Font.ttc', 18)
 
 def init_display():
     #initialize and clear display
@@ -27,6 +26,7 @@ def init_image(epd):
     draw = ImageDraw.Draw(draw_image)
     return draw,draw_image
 
+'''
 def on_key_press(key, draw, epd, current_text):
     try:
         if key == keyboard.Key.enter:
@@ -47,30 +47,25 @@ def capture_and_display_input(draw, epd):
     listener = keyboard.Listener(on_press=lambda key: on_key_press(key, draw, epd, current_text))
     listener.start()
     listener.join()
-
 '''
+
 # Not currently working. This method is displaying some of the CLI for some reason. 
 def display_image(draw, draw_image, epd):
-    logging.info("Keyboard input...")
-    text = "Startup Text"
-    draw.text((10, 10), text, font=font18, fill=0)
-    epd.display_Partial(epd.getbuffer(draw_image))
-
+    current_text = ""
     while True:
-        if keyboard.is_pressed('esc'):
-            break  # Exit on pressing Escape key
-        if keyboard.read_key():
-            key = keyboard.read_key()
-            if key == 'space':
-                key = ' '  # Replace 'space' with a space character
-            elif key == 'a':
-                key = 'a'
-            text += key
-            draw.rectangle((0, 0, 400, 300), fill=255)  # Adjust as needed
-            draw.text((10, 10), text, font=font18, fill=0)
+        key_event = keyboard.read_event()
+        if key_event.event_type == keyboard.KEY_DOWN:
+            key = key_event.name
+            if key == 'enter':
+                current_text += '\n'
+            elif key == 'backspace':
+                current_text = current_text[:-1]
+            elif len(key) == 1:  # Check if the key is a character
+                current_text += key
+
+            draw.rectangle((0, 0, 300, 30), fill=255)
+            draw.text((10, 10), current_text, font=font18, fill=0)
             epd.display_Partial(epd.getbuffer(draw_image))
-            time.sleep(0.1)  # Adjust as needed for responsiveness
-    '''
 
 def cleanup(epd):
     # Cleanup and sleep
@@ -84,8 +79,8 @@ def cleanup(epd):
 try:
     epd = init_display()
     draw, draw_image = init_image(epd)
-    #display_image(draw, draw_image, epd)
-    capture_and_display_input(draw,epd)
+    display_image(draw, draw_image, epd)
+    #capture_and_display_input(draw,epd)
     cleanup(epd)  
 
 except IOError as e:
