d14f1835feac23e18c45e35138685be72f401d96
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-25 21:10:01 -0500

    blahblah

--- partial_update_test copy.py
+++ partial_update_test copy.py
@@ -3,11 +3,17 @@ import os
 import logging
 from waveshare_epd import epd4in2_V2
 from PIL import Image,ImageDraw,ImageFont
+import keyboard
+import time
+import signal
 
 #initialize some vars
 logging.basicConfig(level=logging.DEBUG)
 font18 = ImageFont.truetype('Font.ttc', 11)
 text = ""
+chars_per_line = 45
+lines_on_screen = 25
+current_line = 0
 
 def init_display():
     #initialize and clear display
@@ -24,10 +30,28 @@ def init_image(epd):
     draw = ImageDraw.Draw(draw_image)
     return draw,draw_image
 
+# Not currently working. This method is displaying some of the CLI for some reason. 
+def get_input_text(e):
+    global text
+    text = "init text"
+    logging.info("succesfully read a key")
+    if e.name == 'enter':
+        logging.info("\nKey Pressed:" + e.name)
+        e.name += '\n'
+    elif key == 'backspace':
+        logging.info("\nKey Pressed:" + e.name)
+        text = text[:-1]
+    elif len(e.name) == 1:  # Check if the key is a character
+        logging.info("\nKey Pressed:" + e.name)
+        text += e.name
+    partial_update_text(text)
+    time.sleep(.1)
+    return text
+
 def partial_update_text(draw, draw_image,text, epd):
     logging.info("draw text")
     draw.rectangle((0, 0, 400, 300), fill = 255)
-    draw.text((5, 5), text, font = font18, fill=0)
+    draw.text((0, 0), text, font = font18, fill=0)
     epd.display_Partial(epd.getbuffer(draw_image))
 
 def cleanup(epd):
@@ -36,19 +60,15 @@ def cleanup(epd):
     epd.Clear()
     epd.sleep()
 
+# start keyboard listener and callback to get_input_text method
+keyboard.on_press(get_input_text, suppress=True) #handles keyboard input
 epd = init_display() #initialize the display one time. 
 draw, draw_image = init_image(epd)
 
+'''
 while True:
     try:
-        count = 0
-        while True:
-            if(count < 10):
-                str_count = str(count)
-                text = text + " " + str_count
-                partial_update_text(draw, draw_image, text, epd)
-                count = count + 1
-            break
+        
 
     except IOError as e:
         logging.info(e)
@@ -58,3 +78,4 @@ while True:
         epd4in2_V2.epdconfig.module_exit(cleanup=True)
         cleanup(epd) 
         exit()
+'''
