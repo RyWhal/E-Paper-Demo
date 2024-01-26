9704b9a20f5bb771f814250b206b2252085f6bc3
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-25 20:22:25 -0500

    Update typing_example.py

--- typing_example.py
+++ typing_example.py
@@ -7,12 +7,9 @@ import keyboard
 import time
 import signal
 
+#initialize some vars
 logging.basicConfig(level=logging.DEBUG)
-#logging.info("epd4in2 Demo")
-
-#logging.info("set font object")
-font18 = ImageFont.truetype('Font.ttc', 18)
-
+font18 = ImageFont.truetype('Font.ttc', 11)
 text = ""
 
 
@@ -33,23 +30,16 @@ def init_image(epd):
 
 # Not currently working. This method is displaying some of the CLI for some reason. 
 def get_input_text(e):
-    logging.info("Enter display_image()")
     global text
     text = "init text"
-    #while True:
-    #logging.info("enter display_image while loop")
-    #key_event = keyboard.read_event()
-    #if key_event.event_type == keyboard.KEY_DOWN:
-    #key = key_event.name
-    key = e.name
     logging.info("succesfully read a key")
-    if key == 'enter':
+    if e.name == 'enter':
         logging.info("\nKey Pressed:" + e.name)
-        text += '\n'
+        e.name += '\n'
     elif key == 'backspace':
         logging.info("\nKey Pressed:" + e.name)
         text = text[:-1]
-    elif len(key) == 1:  # Check if the key is a character
+    elif len(e.name) == 1:  # Check if the key is a character
         logging.info("\nKey Pressed:" + e.name)
         text += e.name
     time.sleep(.1)
@@ -71,10 +61,10 @@ def cleanup(epd):
 keyboard.on_press(get_input_text, suppress=True) #handles keyboard input
 epd = init_display() #initialize the display one time. 
 draw, draw_image = init_image(epd)
-epd.init_fast(epd.Seconds_1_5S)
 
 while True:
     try:
+        print(text)
         partial_update_text(draw, draw_image, text, epd)
         time.sleep(6)
         #capture_and_display_input(draw,epd) 
