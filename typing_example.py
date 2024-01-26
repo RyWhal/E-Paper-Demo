963933534f0455ec8079e42217978c181e18d45e
Ryan Whalen <RyWhal@users.noreply.github.com>
2024-01-25 19:58:14 -0500

    Update typing_example.py

--- typing_example.py
+++ typing_example.py
@@ -68,13 +68,13 @@ def get_input_text(e):
     key = e.name
     logging.info("succesfully read a key")
     if key == 'enter':
-        logging.info(e.name)
+        logging.info("\nKey Pressed:" + e.name)
         text += '\n'
     elif key == 'backspace':
-        logging.info(e.name)
+        logging.info("\nKey Pressed:" + e.name)
         text = text[:-1]
     elif len(key) == 1:  # Check if the key is a character
-        logging.info("key pressed" + e.name)
+        logging.info("\nKey Pressed:" + e.name)
         text += e.name
     time.sleep(.05)
     return text
